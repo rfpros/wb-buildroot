f7ed7ee45473166ea6b17326420b962a9a33fda1
Dan Kephart <dan.kephart@lairdtech.com>
2015-05-11 17:11:53 -0400

    Revert "Make external toolchain relocatable"
    This reverts commit befb9a3ee0c5994162f527d8ef763d57d68ec48e.
    
    Remove the buildroot change that makes the buildroot external toolchain
    wrapper relocatable because it is buggy. When adding the that external
    oolchian wrapper to the PATH, it can't find its sysroot:
    http://lists.busybox.net/pipermail/buildroot/2013-February/067402.html
    
    Conflicts:
    	package/Makefile.in
    	toolchain/toolchain-external/ext-tool.mk
    	toolchain/toolchain-external/ext-toolchain-wrapper.c
    	toolchain/toolchain-external/toolchain-external.mk

--- toolchain/toolchain-external/ext-toolchain-wrapper.c
+++ toolchain/toolchain-external/ext-toolchain-wrapper.c
@@ -2,12 +2,9 @@
  * Buildroot wrapper for external toolchains. This simply executes the real
  * toolchain with a number of arguments (sysroot/arch/..) hardcoded,
  * to ensure the external toolchain uses the correct configuration.
- * The hardcoded path arguments are defined relative to the actual location
- * of the binary.
  *
  * (C) 2011 Peter Korsgaard <jacmet@sunsite.dk>
  * (C) 2011 Daniel Nyström <daniel.nystrom@timeterminal.se>
- * (C) 2012 Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
  * (C) 2013 Spenser Gilliland <spenser@gillilanding.com>
  *
  * This file is licensed under the terms of the GNU General Public License
@@ -23,8 +20,7 @@
 #include <stdlib.h>
 #include <errno.h>
 
-static char path[PATH_MAX];
-static char sysroot[PATH_MAX];
+static char path[PATH_MAX] = BR_CROSS_PATH;
 
 /**
  * GCC errors out with certain combinations of arguments (examples are
@@ -41,7 +37,7 @@ static char sysroot[PATH_MAX];
 
 static char *predef_args[] = {
 	path,
-	"--sysroot", sysroot,
+	"--sysroot", BR_SYSROOT,
 #ifdef BR_ABI
 	"-mabi=" BR_ABI,
 #endif
@@ -71,6 +67,19 @@ static char *predef_args[] = {
 #endif
 };
 
+static const char *get_basename(const char *name)
+{
+	const char *base;
+
+	base = strrchr(name, '/');
+	if (base)
+		base++;
+	else
+		base = name;
+
+	return base;
+}
+
 static void check_unsafe_path(const char *path, int paranoid)
 {
 	char **c;
@@ -93,63 +102,10 @@ static void check_unsafe_path(const char *path, int paranoid)
 int main(int argc, char **argv)
 {
 	char **args, **cur;
-	char *relbasedir, *absbasedir;
-	char *progpath = argv[0];
-	char *basename;
 	char *env_debug;
 	char *paranoid_wrapper;
 	int paranoid;
 	int ret, i, count = 0, debug;
-
-	/* Calculate the relative paths */
-	basename = strrchr(progpath, '/');
-	if (basename) {
-		*basename = '\0';
-		basename++;
-		relbasedir = malloc(strlen(progpath) + 7);
-		if (relbasedir == NULL) {
-			perror(__FILE__ ": malloc");
-			return 2;
-		}
-		sprintf(relbasedir, "%s/../..", argv[0]);
-		absbasedir = realpath(relbasedir, NULL);
-	} else {
-		basename = progpath;
-		absbasedir = malloc(PATH_MAX + 1);
-		ret = readlink("/proc/self/exe", absbasedir, PATH_MAX);
-		if (ret < 0) {
-			perror(__FILE__ ": readlink");
-			return 2;
-		}
-		absbasedir[ret] = '\0';
-		for (i = ret; i > 0; i--) {
-			if (absbasedir[i] == '/') {
-				absbasedir[i] = '\0';
-				if (++count == 3)
-					break;
-			}
-		}
-	}
-	if (absbasedir == NULL) {
-		perror(__FILE__ ": realpath");
-		return 2;
-	}
-
-	/* Fill in the relative paths */
-#ifdef BR_CROSS_PATH_REL
-	ret = snprintf(path, sizeof(path), "%s/" BR_CROSS_PATH_REL "/%s", absbasedir, basename);
-#else /* BR_CROSS_PATH_ABS */
-	ret = snprintf(path, sizeof(path), BR_CROSS_PATH_ABS "/%s", basename);
-#endif
-	if (ret >= sizeof(path)) {
-		perror(__FILE__ ": overflow");
-		return 3;
-	}
-	ret = snprintf(sysroot, sizeof(sysroot), "%s/" BR_SYSROOT, absbasedir);
-	if (ret >= sizeof(sysroot)) {
-		perror(__FILE__ ": overflow");
-		return 3;
-	}
 
 	cur = args = malloc(sizeof(predef_args) +
 			    (sizeof(char *) * (argc + EXCLUSIVE_ARGS)));
@@ -230,6 +186,7 @@ int main(int argc, char **argv)
 
 	/* finish with NULL termination */
 	*cur = NULL;
+	strcat(path, get_basename(argv[0]));
 
 	/* Debug the wrapper to see actual arguments passed to
 	 * the compiler:
