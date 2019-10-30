194d05eb00a9946c8c0f5c15026424be60b86fb4
Adam Ruehl <adam.ruehl@lairdtech.com>
2019-10-30 11:37:50 -0400

    laird: add Semtech Basic Station to buildroot.
    add Semtech Basic Station to buildroot.
    
    Bug 15303

---
+++ package/lrd/basicstation/basicstation.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# Semtech Basic Station
+#
+################################################################################
+BASICSTATION_VERSION = c29b8502f8c715daecec6666835da6e981dc820a
+BASICSTATION_SITE = https://github.com/lorabasics/basicstation.git
+BASICSTATION_SITE_METHOD = git
+BASICSTATION_DEPENDENCIES = mbedtls libloragw
+
+define  BASICSTATION_BUILD_CMDS
+	CC="$(TARGET_CC)" AR="$(TARGET_AR)" ARCH=arm-laird-linux-gnueabi BUILD_DIR=$(@D) BR_LDFLAGS="-L $(STAGING_DIR)/usr/lib -L $(STAGING_DIR)/lib -lm -lpthread -lmbedtls -lmbedx509 -lmbedcrypto" LGW_PATH="$(STAGING_DIR)/usr/lib/libloragw" MBEDTLS_PATH="$(STAGING_DIR)/usr" $(MAKE) -C $(@D)
+endef
+
+define BASICSTATION_INSTALL_TARGET_CMDS
+mkdir -p $(TARGET_DIR)/opt/lora/basicstation
+$(INSTALL) -m 0755 $(@D)/build-laird-std/bin/station $(TARGET_DIR)/opt/lora/basicstation/station
+endef
+
+$(eval $(generic-package))
+
