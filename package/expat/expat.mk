08b7d14467fe3bf3c437c74f41610ffc6c28b71e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-09-21 23:07:33 +0200

    package/expat: bump to version 2.4.9
    - Drop patch (akready in version)
    - Update hash of COPYING (year updated with
      https://github.com/libexpat/libexpat/commit/39b2e99355039626438d48a4eca6aacba2b44d5e)
    
    https://blog.hartwork.org/posts/expat-2-4-9-released
    https://github.com/libexpat/libexpat/blob/R_2_4_9/expat/Changes
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,15 +4,12 @@
 #
 ################################################################################
 
-EXPAT_VERSION = 2.4.8
+EXPAT_VERSION = 2.4.9
 EXPAT_SITE = http://downloads.sourceforge.net/project/expat/expat/$(EXPAT_VERSION)
 EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.xz
 EXPAT_INSTALL_STAGING = YES
 EXPAT_LICENSE = MIT
 EXPAT_LICENSE_FILES = COPYING
-
-# 0001-Ensure-raw-tagnames-are-safe-exiting-internalEntityParser.patch
-EXPAT_IGNORE_CVES += CVE-2022-40674
 
 EXPAT_CONF_OPTS = \
 	--without-docbook --without-examples --without-tests --without-xmlwf
